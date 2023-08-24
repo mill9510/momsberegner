efcdf39ea1f9b759f541364e80a8c47fc88c569f
Mill9510 <mill9510@stud.kea.dk>
2023-08-24 16:07:43 +0200

    moms i console

---
+++ script.js
@@ -0,0 +1,6 @@
+function beregnMoms(beloeb, moms = 25) {
+  //   let beloeb = parseInt(document.querySelector("input").value);
+  console.log(beloeb + (beloeb / 100) * moms + " med moms");
+}
+
+beregnMoms(100);
